1596ba0cd966982706c27b09888bc60f303e2a4f
Mikenziokah <mikenziokah@gmail.com>
2024-06-30 13:46:17 +0300

    admin configuration template

--- library/admin.py
+++ library/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Book,StudentExtra,IssuedBook
-# Register your models here.
+
 class BookAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Book, BookAdmin)
